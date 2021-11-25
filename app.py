72d552f36a9cf5b3b748499ce3a3f1ed842fa8ba
farit <f.galeev@innopolis.ru>
2021-11-26 00:04:45 +0300

    add voice handler

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import sys
-from flask import Flask, redirect, request
+from flask import Flask, redirect, request, jsonify
+import requests
+from flask_cors import CORS
 import os
 import random
+import urllib.request
+import json
 
 sys.path.append("feed_forward_vqgan_clip")
 from src.vqgan_clip import load_gpt_model, load_vqgan_model, load_perceptor, generate_sentence, generate_video
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+CORS(app, resources={r"/*": {'origins': '*'}})
 
 
 @app.route("/")
@@ -30,8 +37,37 @@ def generate(start_story):
     return generated_sentence
 
 
+@app.route("/get_audio", methods=["POST"])
+def get_audio():
+    print(os.environ.get('IAM_TOKEN'))
+    print(os.environ.get('FOLDER_ID'))
+    data = request.data
+    with open("voice.opus", "wb") as f:
+        f.write(data)
+
+    os.system("ffmpeg -i voice.opus -f wav - | oggenc -o voice.ogg -")
+
+    with open("voice.ogg", "rb") as f:
+        data = f.read()
+    responseData = requests.post("https://stt.api.cloud.yandex.net/speech/v1/stt:recognize",
+                  params={
+                      "topic": "general",
+                      "folderId": os.environ.get('FOLDER_ID'),
+                      "lang": "ru-RU",
+                      "profanityFilter": True
+                  },
+                  headers={
+                      "Authorization": f"Bearer {os.environ.get('IAM_TOKEN')}",
+                      "Content-Type": "audio/ogg"
+                  }, data=data)
+
+    decodedData = responseData.json()
+    return decodedData
+
+
 if __name__ == '__main__':
     tokenizer, gpt3 = load_gpt_model("rugpt3small_based_on_gpt2")
     vqgan_model, mlp_mixer, _ = load_vqgan_model("feed_forward_vqgan_clip/cc12m_32x1024_mlp_mixer.th")
     perceptor = load_perceptor()
-    app.run()
+    app.run(debug=True, load_dotenv=True)
+
