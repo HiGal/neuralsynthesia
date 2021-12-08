ee54e0c43bf3dab5fad6b6545d1613b771db27d8
farit <f.galeev@innopolis.ru>
2021-12-08 21:19:57 +0300

    pre-final version

---
+++ frontend/src/components/Jumbotron.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+  <b-jumbotron header-tag="h3" bg-variant="transparent" text-variant="white"  >
+    <template #header>Нейросказки</template>
+    <template #lead>
+      Начните рассказ, а нейросеть продолжит её за вас и сгенрирует видеопредставление по сгенерированному тексту*
+    </template>
+
+    <hr class="my-2">
+
+    <p>
+      <i>*Тексты и видео иногда могут быть очень абстрактными и лишенными смысла</i>
+    </p>
+
+  </b-jumbotron>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: "Jumbotron"
+}
+</script>
+
+<style scoped>
+
+</style>
